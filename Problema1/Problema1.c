5ba023692ab6dc6d389595ad06cd147f1605d358
ShannonNCM <shannyotaku@gmail.com>
2020-04-26 12:37:34 -0600

    version final

--- Problema1/Problema1.c
+++ Problema1/Problema1.c
@@ -5,8 +5,8 @@ Compilado: gcc Problema1.c -o Problema1.c
 Fecha: sáb abr 25 11:02:30 CST 2020
 Librerias: stdio, etc.
 Resumen: El siguiente programa realizar una grafica que compara los valores de tension y longitud de onda y la recta que mejor aproxima el comportaiento de los mismo, asi como calcula la densidad lineal de la cuerda y la longitud de onda cuando la tension es 6N.
-Entradas: no se
-Salidas: no se
+Entradas: Vectores con decimales
+Salidas: Decimales
 */
   
 //Librerias
